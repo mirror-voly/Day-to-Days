e601d621aa0a42bda496d761e604c8ade54f5afc
nur <77068320+mirror-voly@users.noreply.github.com>
2024-09-03 15:03:15 +0400

    Initial Commit

---
+++ Day to Days/Day_to_DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_to_DaysApp.swift
+//  Day to Days
+//
+//  Created by mix on 03.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Day_to_DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
