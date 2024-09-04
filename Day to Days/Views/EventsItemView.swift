4e7ac2a5e9ef4d65b5e42c02fef5e2a660f35503
nur <77068320+mirror-voly@users.noreply.github.com>
2024-09-04 16:07:23 +0400

    UI updates

--- Day to Days/Views/EventsItemView.swift
+++ Day to Days/Views/EventsItemView.swift
@@ -9,31 +9,55 @@ import SwiftUI
 
 struct EventsItemView: View {
     let day: Day
+    @Environment(\.colorScheme) var colorScheme
     var body: some View {
+        let haloColor: Color = colorScheme == .light ? .white : .black
         HStack {
-            Circle()
-                .fill(day.color.opacity(0.9))
-                .overlay(Circle()
-                    .fill(.white)
-                    .frame(width: 20, height: 10))
-                .frame(width: 20, height: 20)
-            Text(day.title)
-                .font(.title2)
-            Spacer()
-            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
-                let dateData = DateCalculator.daysFrom(thisDate: day.date)
-                Text(dateData.days)
-                    .foregroundStyle(day.color)
-                    .bold()
-                Group {
-                    Text("days")
-                    Text(dateData.description)
-                }
-                .italic()
-                .font(.footnote)
-                .foregroundStyle(.gray)
+            ZStack(alignment: .center, content: {
+                Circle()
+                    .fill(Color.white.opacity(0.5))
+                    .frame(width: 22, height: 22)
+                    Circle()
+                        .fill(day.color)
+                        .overlay(
+                            Circle()
+                            .fill(haloColor)
+                            .frame(width: 20, height: 15))
+                        .frame(width: 20, height: 20)
             })
+            .frame(width: 25)
+            .padding()
+                Text(day.title)
+                    .font(.title2)
+            Spacer()
+            GroupBox {
+                VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+                    let dateData = DateCalculator.daysFrom(thisDate: day.date)
+                    GroupBox {
+                        ZStack(alignment: .center, content: {
+                            Circle()
+                                .fill(Color.white)
+                                .frame(width: 50, height: 50)
+                            Text(dateData.days)
+                                .foregroundStyle(day.color)
+                                .bold()
+                                .font(.title3)
+                                .frame(width: 40)
+                                .minimumScaleFactor(0.5)
+                                .lineLimit(1)
+                        })
+                        .frame(width: 40, height: 40)
+                    }
+                    Group {
+                        Text("days")
+                        Text(dateData.description)
+                    }
+                    .italic()
+                    .font(.footnote)
+                    .foregroundStyle(.gray)
+                })
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 10))
         }
-        
     }
 }
