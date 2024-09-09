83cc7f015d8ad382fe6ac755525a4cd1ef72bc0d
nur <77068320+mirror-voly@users.noreply.github.com>
2024-09-09 10:38:00 +0400

    Slider minor improvements

--- Day to Days/ViewModel/DataStore.swift
+++ Day to Days/ViewModel/DataStore.swift
@@ -40,7 +40,10 @@ final class DataStore {
 
     private func addUITestData() {
         allEvents.append(contentsOf: [Event(title: "My first try", description: "", date: ISO8601DateFormatter().date(from: "2025-03-15T14:30:00Z")!, dateType: .day, color: .red),
-                                      Event(title: "Meeting", description: "well it something", date: ISO8601DateFormatter().date(from: "2024-03-11T14:30:00Z")!, dateType: .day, color: .teal),
+                                      Event(title: "Meeting", description: "well it something", date: ISO8601DateFormatter().date(from: "2020-03-11T14:30:00Z")!, dateType: .year, color: .teal),
+                                      Event(title: "Meeting", description: "well it something", date: ISO8601DateFormatter().date(from: "2024-03-11T14:30:00Z")!, dateType: .day, color: .indigo),
+                                      Event(title: "Meeting", description: "well it something", date: ISO8601DateFormatter().date(from: "2022-01-11T14:30:00Z")!, dateType: .month, color: .brown),
+                                      Event(title: "Meeting", description: "well it something", date: ISO8601DateFormatter().date(from: "2027-06-11T14:30:00Z")!, dateType: .weak, color: .blue),
                                       Event(title: "Paiment for rent", description: "best day", date: ISO8601DateFormatter().date(from: "2021-03-15T14:30:00Z")!, dateType: .day, color: .pink)])
     }
 
