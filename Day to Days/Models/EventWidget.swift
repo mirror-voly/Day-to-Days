8d03e6a788f14ec478f07d27a628ff4154364d13
nur <77068320+mirror-voly@users.noreply.github.com>
2024-09-22 11:28:58 +0400

    Models updated

--- Day to Days/Models/EventWidget.swift
+++ Day to Days/Models/EventWidget.swift
@@ -11,10 +11,17 @@ final class EventWidget: Codable {
     let name: String
     let id: UUID
     let date: Date
+    var dateTypeData: Data?
 
-    init(name: String, id: UUID, date: Date) {
+    var dateType: DateType {
+        guard let data = dateTypeData else { return .day}
+        return DateType.fromData(data) ?? .day
+    }
+
+    init(name: String, id: UUID, date: Date, dateType: DateType) {
         self.name = name
         self.id = id
         self.date = date
+        self.dateTypeData = dateType.toData()
     }
 }
